e19aaaf39dbaf489d0c3f987096f7208e6b73e30
Z4 <z4kreyyt@gmail.com>
2024-01-24 14:33:51 +0000

    Adding Tawk

--- js/index.js
+++ js/index.js
@@ -1,3 +1,14 @@
+
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/65b11fa40ff6374032c46206/1hktu76sl';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+
 document.ondragstart = () => false;
 
 const loadContent = () => {
