cebe145e85a9a5a632266d9af942dd026facc62b
Z4 <z4kreyyt@gmail.com>
2024-02-02 22:49:59 +0000

    feat: Modal + students links

--- js/index.js
+++ js/index.js
@@ -1,33 +1,60 @@
 document.ondragstart = () => false;
 
 const loadContent = () => {
-    anime({
-        targets: 'body',
-        opacity: 1,
-        duration: 2500,
-        easing: 'easeInOutQuad'
+  anime({
+    targets: "body",
+    opacity: 1,
+    duration: 2500,
+    easing: "easeInOutQuad",
+  });
 
-    })
+  anime({
+    targets: ".button",
+    scale: 1,
+    duration: 2800,
+    easing: "easeInOutQuad",
+  });
 
-    anime({
-        targets: '.button',
-        scale: 1,
-        duration: 2800,
-        easing: 'easeInOutQuad'
-    })
+  anime({
+    targets: ".imagescontainer",
+    loop: true,
+    duration: 3000,
+    keyframes: [{ translateY: -20 }, { translateY: 0 }],
+    easing: "easeInOutSine",
+  });
+};
 
-    anime({
-        targets: '.imagescontainer',
-        loop: true,
-        duration: 3000,
-        keyframes: [
-            { translateY: -20 },
-            { translateY: 0 }
-        ],
-        easing: 'easeInOutSine'
-    })
+function Download(type) {
+  switch (type) {
+    case "Admin":
+      window.open(
+        "https://github.com/NetSafeGuard/NSG_ADMIN/releases/download/0.0.1/nsg_0.0.0_x64_en-US.msi"
+      );
+      break;
+    case "Students":
+      window.open(
+        "https://github.com/NetSafeGuard/NSG_STUDENTS/releases/download/untagged-b221c35beef1a78a62d3/nsgtest.exe"
+      );
+      break;
+    case "Dependency":
+      window.open("https://www.winpcap.org/install/bin/WinPcap_4_1_3.exe");
+      break;
+  }
 }
 
-function DOWNLOAD() {
-    window.open("https://github.com/NetSafeGuard/NSG_ADMIN/releases/download/0.0.1/nsg_0.0.0_x64_en-US.msi")
+let isModalOpen = false;
+
+function toggleModal() {
+  if (isModalOpen) {
+    console.log("closing modal");
+    document.getElementById("warning").style.display = "none";
+    document.body.style.overflowY = "auto";
+
+    isModalOpen = false;
+    return;
+  }
+
+  isModalOpen = true;
+  document.getElementById("warning").style.display = "flex";
+  document.body.style.overflow = "hidden";
 }
